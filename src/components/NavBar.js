3729cadecabd0a349297c0de564067a8286b40eb
wang0805 <wh_wang@live.com>
2020-09-17 15:33:21 +0800

    test2

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -139,67 +139,9 @@ class NavBar extends Component {
               </div>
             </div>
             {/*=======  End of header top bar  =======*/}
-            <div className="header-info-area">
-              <div className="container">
-                <div className="row align-items-center">
-                  <div className="col-lg-12">
-                    <div className="header-info-wrapper align-items-center">
-                      {/* logo */}
-                      <div className="logo">
-                        <Link to={`${process.env.PUBLIC_URL}/`}>
-                          <img
-                            src="assets/img/logo/bpi_logo.png"
-                            className="img-fluid"
-                            alt="Logo"
-                            width="110"
-                          />
-                        </Link>
-                      </div>
-                      {/* header contact info */}
-                      <div className="header-contact-info">
-                        <div className="header-info-single-item">
-                          <div className="header-info-single-item__icon">
-                            <i className="zmdi zmdi-smartphone-android" />
-                          </div>
-                          <div className="header-info-single-item__content">
-                            <h6 className="header-info-single-item__title">
-                              {/* {t("phone")} */}
-                              Email
-                            </h6>
-                            <p className="header-info-single-item__subtitle">
-                              ops@bpi-trading.com
-                            </p>
-                          </div>
-                        </div>
-                        <div className="header-info-single-item">
-                          <div className="header-info-single-item__icon">
-                            <i className="zmdi zmdi-home" />
-                          </div>
-                          <div className="header-info-single-item__content">
-                            <h6 className="header-info-single-item__title">
-                              {t("address")}
-                            </h6>
-                            <p className="header-info-single-item__subtitle">
-                              Springleaf Tower #19-01 Singapore 079909
-                            </p>
-                          </div>
-                        </div>
-                      </div>
-                      {/* mobile menu */}
-                      <div
-                        className="mobile-navigation-icon"
-                        id="mobile-menu-trigger"
-                        onClick={this.activeMobileMenu}
-                      >
-                        <i />
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+
             {/*=======  header info area  =======*/}
-            {/* <div
+            <div
               style={{
                 position: "fixed",
                 top: 20,
@@ -214,7 +156,7 @@ class NavBar extends Component {
               >
                 <i />
               </div>
-            </div> */}
+            </div>
             {/*=======  End of header info area =======*/}
             {/*=======  header navigation area  =======*/}
             <div className="header-navigation-area default-bg">
---
+++ src/components/test.js
@@ -0,0 +1,59 @@
+{/* <div className="header-info-area">
+  <div className="container">
+    <div className="row align-items-center">
+      <div className="col-lg-12">
+        <div className="header-info-wrapper align-items-center">
+          {/* logo */}
+          <div className="logo">
+            <Link to={`${process.env.PUBLIC_URL}/`}>
+              <img
+                src="assets/img/logo/bpi_logo.png"
+                className="img-fluid"
+                alt="Logo"
+                width="110"
+              />
+            </Link>
+          </div>
+          {/* header contact info */}
+          <div className="header-contact-info">
+            <div className="header-info-single-item">
+              <div className="header-info-single-item__icon">
+                <i className="zmdi zmdi-smartphone-android" />
+              </div>
+              <div className="header-info-single-item__content">
+                <h6 className="header-info-single-item__title">
+                  {/* {t("phone")} */}
+                  Email
+                </h6>
+                <p className="header-info-single-item__subtitle">
+                  ops@bpi-trading.com
+                </p>
+              </div>
+            </div>
+            <div className="header-info-single-item">
+              <div className="header-info-single-item__icon">
+                <i className="zmdi zmdi-home" />
+              </div>
+              <div className="header-info-single-item__content">
+                <h6 className="header-info-single-item__title">
+                  {t("address")}
+                </h6>
+                <p className="header-info-single-item__subtitle">
+                  Springleaf Tower #19-01 Singapore 079909
+                </p>
+              </div>
+            </div>
+          </div>
+          {/* mobile menu */}
+          <div
+            className="mobile-navigation-icon"
+            id="mobile-menu-trigger"
+            onClick={this.activeMobileMenu}
+          >
+            <i />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>; */}
