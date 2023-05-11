6f56f8493f7f46d73bb14cff2dc9259e5d610629
wang0805 <wh_wang@live.com>
2023-05-12 00:49:33 +0800

    test changing overflow

--- src/components/Brochure.js
+++ src/components/Brochure.js
@@ -4,7 +4,7 @@ import brochurePDF from './../../public/assets/pdf/brochure.pdf';
 class Brochure extends Component {
     render(){
         return(
-            <div style={{height:'100vh'}}>
+            <div style={{height:'1500px', overflowY: "scroll"}}>
                 <embed src={brochurePDF} type="application/pdf" width="100%" height="100%"/>
             </div>
               
