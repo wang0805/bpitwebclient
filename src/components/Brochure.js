f876cd487a0081a571478e1678d82d25d4d84359
wang wenhao <33215115+wang0805@users.noreply.github.com>
2023-05-12 10:54:23 +0800

    Update Brochure.js

--- src/components/Brochure.js
+++ src/components/Brochure.js
@@ -1,11 +1,10 @@
 import React, { Component } from "react";
 import brochurePDF from './../../public/assets/pdf/brochure.pdf';
-
+//<embed src={brochurePDF} type="application/pdf" width="100%" height="100%"/>
 class Brochure extends Component {
     render(){
         return(
             <div style={{height:'100vh'}}>
-//                 <embed src={brochurePDF} type="application/pdf" width="100%" height="100%"/>
                 <object data={brochurePDF} type="application/pdf" width="100%" height="100%">
                     <p>Unable to display PDF file. <a href={brochurePDF}>Download</a> instead.</p>
                 </object>
