58993fe328cdf1f5e91915e6db23981a30f3eb6b
wang wenhao <33215115+wang0805@users.noreply.github.com>
2023-05-12 10:50:18 +0800

    Update Brochure.js

--- src/components/Brochure.js
+++ src/components/Brochure.js
@@ -5,7 +5,10 @@ class Brochure extends Component {
     render(){
         return(
             <div style={{height:'100vh'}}>
-                <embed src={brochurePDF} type="application/pdf" width="100%" height="100%"/>
+//                 <embed src={brochurePDF} type="application/pdf" width="100%" height="100%"/>
+                <object data={brochurePDF} type="application/pdf" width="100%" height="100%">
+                    <p>Unable to display PDF file. <a href={brochurePDF}>Download</a> instead.</p>
+                </object>
             </div>
               
             
